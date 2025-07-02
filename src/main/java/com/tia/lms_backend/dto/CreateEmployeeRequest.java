312ebb01432fc8bb9c50242076fe5c0d9760c0fd
tahaakocer <taha.kocer317@gmail.com>
2025-07-02 11:50:31 +0300

    created employee service

--- src/main/java/com/tia/lms_backend/dto/CreateEmployeeRequest.java
+++ src/main/java/com/tia/lms_backend/dto/CreateEmployeeRequest.java
@@ -1,4 +1,29 @@
 package com.tia.lms_backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CreateEmployeeRequest {
+
+    private String name;
+    private String lastName;
+    private String email;
+    private String birthDate;
+    private String tckn;
+
+    private String education;
+    private String title;
+
+    private String teamId;
+    private String departmentId;
 }
